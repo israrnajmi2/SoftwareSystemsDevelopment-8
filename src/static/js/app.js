698c340d8bdfb759280fa75e7b717ad77ad0e637
israrnajmi2 <israrnajmi2@gmail.com>
2025-01-21 01:16:18 +0900

    Fixed test issues and updated ToDo app title

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -4,6 +4,7 @@ function App() {
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
+                <h1 className="text-center">ToDo App - Israr Khaliq</h1>
                     <TodoListCard />
                 </Col>
             </Row>
